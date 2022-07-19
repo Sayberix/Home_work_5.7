24bebfcd43df14263f357faab73aedb4639d0fef
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-19 22:46:46 +0700

    начало работы над задачей 5.7.2

--- Home_work_5.7/Program.cs
+++ Home_work_5.7/Program.cs
@@ -4,35 +4,16 @@
 
     internal class Program
     {
-        // Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-        // m = 3, n = 4.
-        // 0,5 7 -2 -0,2
-        // 1 -3,3 8 -9,9
-        // 8 7,8 -7,1 9
-
-        // Вариант решения задачи одной функцией. Решение с двумя функциями можно посмотреть в коммите e62ef2a - git checkout e62ef2a
+        // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+        // Например, задан массив:
+        // 1 4 7 2
+        // 5 9 2 3
+        // 8 4 2 4
+        // 1,7 -> такой позиции в массиве нет
 
         private static void Main(string[] args)
         {
-            int m = 3, n = 4;
-            double[,] array = new double[m, n];
-            FillandOutputArray(array);
-
-            // функция заполнения и вывода массива произольными вещественными числами
-            void FillandOutputArray(double[,] ArrayInFunction)
-            {
-                Console.WriteLine("Вывод массива, с произвольно генерируемыми вещественными числами:");
-                for (int i = 0; i < ArrayInFunction.GetLength(0); i++)
-                {
-                    for (int j = 0; j < ArrayInFunction.GetLength(1); j++)
-                    {
-                        Random autoRand = new Random();
-                        ArrayInFunction[i, j] = Math.Round(autoRand.NextDouble() * 10,2);
-                        Console.Write(ArrayInFunction[i, j] + "\t");
-                    }
-                    Console.Write("\n");
-                }
-            }
+ 
         }
     }
 }
