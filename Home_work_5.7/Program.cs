43c32146bb01c1176e07d84d1010d91c178ab360
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-19 21:40:21 +0700

    начало работы над дз 5.7

---
+++ Home_work_5.7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Home_work_5._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ДЗ к уроку № 7. Как не нужно писать код. Часть 1
+        }
+    }
+}
